3758226a12ce3ac38eafb9a4bbf1f244f713df4b
santoshpawar02 <79916335+santoshpawar02@users.noreply.github.com>
2025-03-08 08:38:28 +0530

    santoshpawar.online update

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 git pull
-ansible-playbook -i $1-dev.rdevopsb83.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 roboshop.yml -e component_name=$1
+ansible-playbook -i $1-dev.santoshpawar.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 roboshop.yml -e component_name=$1
